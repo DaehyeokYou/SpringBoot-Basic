5538c247cdf03e04b7bd6e51818b5ad04b03264a
DaehyeokYou <ydh4085@naver.com>
2020-12-22 21:53:40 +0900

    spring jpa, aop, test

--- src/main/java/com/ydh/bootsample/repository/MemberRepository.java
+++ src/main/java/com/ydh/bootsample/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import com.ydh.bootsample.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemeberRepository {
+public interface MemberRepository {
 
     Member save(Member member);
     Optional<Member> findById(Long id);
